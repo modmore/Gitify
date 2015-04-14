40c6bd4bf9e58dd809969d4a7f966bfd834ed10d
Mark Hamstra <hello@markhamstra.com>
2015-04-14 18:22:00 +0200

    - Extract instantiation logic out of Gitify file into application.php for easier integration with non-CLI PHP.

---
+++ application.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Make sure dependencies have been installed, and load the autoloader.
+ */
+if (!file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    throw new \Exception('Uh oh, it looks like dependencies have not yet been installed with Composer. Please follow Please follow the installation instructions at https://github.com/modmore/Gitify/wiki/1.-Installation');
+}
+require dirname(__FILE__) . '/vendor/autoload.php';
+
+/**
+ * Ensure the timezone is set; otherwise you'll get a shit ton (that's a technical term) of errors.
+ */
+if (version_compare(phpversion(),'5.3.0') >= 0) {
+    $tz = @ini_get('date.timezone');
+    if (empty($tz)) {
+        date_default_timezone_set(@date_default_timezone_get());
+    }
+}
+
+/**
+ * Specify the working directory, if it hasn't been set yet.
+ */
+if (!defined('GITIFY_WORKING_DIR')) {
+    define ('GITIFY_WORKING_DIR', $cwd = getcwd() . DIRECTORY_SEPARATOR);
+}
+
+/**
+ * Load all the commands and create the Gitify instance
+ */
+use modmore\Gitify\Gitify;
+use modmore\Gitify\Command\BackupCommand;
+use modmore\Gitify\Command\BuildCommand;
+use modmore\Gitify\Command\ExtractCommand;
+use modmore\Gitify\Command\InitCommand;
+use modmore\Gitify\Command\InstallModxCommand;
+use modmore\Gitify\Command\InstallPackageCommand;
+use modmore\Gitify\Command\RestoreCommand;
+
+$application = new Gitify('Gitify', '0.7.0');
+$application->add(new InitCommand);
+$application->add(new BuildCommand);
+$application->add(new ExtractCommand);
+$application->add(new InstallModxCommand);
+$application->add(new InstallPackageCommand);
+$application->add(new BackupCommand);
+$application->add(new RestoreCommand);
+/**
+ * We return it so the CLI controller in /Gitify can run it, or for other integrations to
+ * work with the Gitify api directly.
+ */
+return $application;
