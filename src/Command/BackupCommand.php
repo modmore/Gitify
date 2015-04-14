4e9eeaf1930a4a2f19acf888b8b8f8eb6165e90a
Marek Skiba <skibamarek@gmail.com>
2015-04-15 00:32:28 +0200

    Added support for the host parameter in mysqldump

--- src/Command/BackupCommand.php
+++ src/Command/BackupCommand.php
@@ -96,7 +96,7 @@ class BackupCommand extends BaseCommand
 
         $output->writeln('Writing database backup to <info>' . $file . '</info>...');
         $database_password = str_replace("'", '\'', $database_password);
-        exec("mysqldump -u {$database_user} -p'{$database_password}' {$dbase} > {$targetFile} ");
+        exec("mysqldump -u {$database_user} -p'{$database_password}' -h {$database_server} {$dbase} > {$targetFile} ");
         return 0;
     }
 }
